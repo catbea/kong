877a3f3b6277db480326a64ee1cfce91ffd91648
zizi <zhangzihb@gmail.com>
2018-11-14 15:06:45 +0800

    全局样式调整

--- src/App.vue
+++ src/App.vue
@@ -58,27 +58,11 @@ html {
       overflow-y: auto;
       > div {
         position: absolute;
+        width: 100%;
+        height: 100%;
         top: 0;
       }
     }
   }
 }
-
-// #app {
-//   font-family: "Avenir", Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
-// #nav {
-//   padding: 30px;
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
 </style>
