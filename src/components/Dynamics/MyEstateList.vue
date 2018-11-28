7c2d1a4163f8ac79dd1faffaf965c1a2a50ca42b
zizi <zhangzihb@gmail.com>
2018-11-28 21:13:32 +0800

    首页

--- src/components/Dynamics/MyEstateList.vue
+++ src/components/Dynamics/MyEstateList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-estate-list">
-    <title-bar :conf="titleBarConf"></title-bar>
+    <title-bar class="title-container" :conf="titleBarConf"></title-bar>
     <estate-item v-for="(item,index) in list" :key="index" :info="item"></estate-item>
 
   </div>
@@ -28,5 +28,8 @@ export default {
 </script>
 <style lang="less">
 .my-estate-list {
+  // >.title-container{
+  //   padding-top: 10px;
+  // }
 }
 </style>
