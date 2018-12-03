4cabc005618d5deffa54efe242d77cfbb09ad7fa
火勺 <donocare@yeah.net>
2018-12-03 17:20:46 +0800

    树形组件添加

---
+++ src/components/Collapse/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="">
+        <van-collapse v-model="activeNames">
+            <van-collapse-item title="有赞微商城" name="1">提供多样店铺模板，快速搭建网上商城</van-collapse-item>
+            <van-collapse-item title="有赞零售" name="2">网店吸粉获客、会员分层营销、一机多种收款，告别经营低效和客户流失</van-collapse-item>
+            <van-collapse-item title="有赞美业" name="3" disabled>线上拓客，随时预约，贴心顺手的开单收银</van-collapse-item>
+        </van-collapse>
+    </div>
+</template>
+<script>
+export default {
+  components: {},
+  // props: {
+  //   info: []
+  // },
+
+  data() {
+    return {
+      activeNames: ['1']
+    }
+  }
+}
+</script>
+
+<style lang="less">
+</style>
