d04513258552591a04cc7951ee17176c1aa43ebc
火勺 <donocare@yeah.net>
2018-12-04 14:54:48 +0800

    更新模式的数据显示

--- src/views/user/noDisturb/index.vue
+++ src/views/user/noDisturb/index.vue
@@ -1,259 +1,352 @@
 <template>
   <div class="no-disturb-page">
     <div class="no-disturb-page-top">
-      <div class="disturb-box">
-        勿扰模式
+      <div class="disturb-box">勿扰模式
         <van-switch v-model="checked" @change="onSwitch"/>
       </div>
-      <div class="disturb-describe">
-        启用勿扰模式后，在设定时间内将收不到任何推送提醒，请谨慎开启避免影响正常工作
-      </div>
+      <div class="disturb-describe">启用勿扰模式后，在设定时间内将收不到任何推送提醒，请谨慎开启避免影响正常工作</div>
     </div>
     <ul class="no-disturb-page-bottom" v-if="checked">
       <li @click="setHandle" class="cycle-set">
         <p>周期设置</p>
         <div class="right" ref="rr">
           <p class="show-date" v-show="showDate">每天</p>
-         <div class="show-box" v-show="showBox"> 
-           <i v-for="(item,index) in periodList" :key="index" v-show="item.check">{{item.date}}</i>
+          <div class="show-box" v-show="showBox">
+            <i v-for="(item,index) in periodList" :key="index" v-show="item.check">{{item.date}}</i>
           </div>
           <span class="bg_img icon" :style="{backgroundImage:'url('+detaliImg+')'}"></span>
         </div>
         <van-popup v-model="popupShow" position="bottom" :overlay="true">
-         <ol class="period-box">
-           <li v-for="(item,index) in periodList" :key="index" @click.stop="periodTaget(index,item)"> 
-             {{item.date}}
-             <span class="bg_img" :style="{backgroundImage:'url('+checkImg+')'}" v-show="item.check"></span>
-           </li>
-         </ol>
-         <div class="confirm-button" @click.stop="periodHandle">
-           确定
-         </div>
+          <ol class="period-box">
+            <li
+              v-for="(item,index) in periodList"
+              :key="index"
+              @click.stop="periodTaget(index,item)"
+            >
+              {{item.date}}
+              <span
+                class="bg_img"
+                :style="{backgroundImage:'url('+checkImg+')'}"
+                v-show="item.check"
+              ></span>
+            </li>
+          </ol>
+          <div class="confirm-button" @click.stop="periodHandle">确定</div>
         </van-popup>
       </li>
-       <li class="start-time" @click="startHandle"> 
+      <li class="start-time" @click="startHandle">
         <p>开始时间</p>
         <div class="right">
           <i>{{startTime}}</i>
           <span class="bg_img icon" :style="{backgroundImage:'url('+detaliImg+')'}"></span>
         </div>
       </li>
-      <van-popup v-model="startShow" position="bottom" :overlay="true" >
-          <van-datetime-picker
+      <van-popup v-model="startShow" position="bottom" :overlay="true">
+        <van-datetime-picker
           v-model="startDate"
           type="time"
           @confirm="onStartConfirm"
           @cancel="onStartCancel"
         />
-        </van-popup>
-       <li class="end-time" @click="endHandle">
+      </van-popup>
+      <li class="end-time" @click="endHandle">
         <p>结束时间</p>
         <div class="right">
           <i>
-            <span v-show="greater">次日</span> {{endTime}}
-            </i>
+            <span v-show="greater">次日</span>
+            {{endTime}}
+          </i>
           <span class="bg_img icon" :style="{backgroundImage:'url('+detaliImg+')'}"></span>
         </div>
       </li>
       <van-popup v-model="endShow" position="bottom" :overlay="true">
-          <van-datetime-picker
+        <van-datetime-picker
           v-model="endDate"
           type="time"
           @confirm="onEndConfirm"
           @cancel="onEndCancel"
         />
-        </van-popup>
+      </van-popup>
     </ul>
   </div>
 </template>
 <script>
+import userService from 'SERVICE/userService'
 export default {
-  data:()=>({
-    checked:false,
-    detaliImg:require('IMG/user/arr.png'),
-    popupShow:false,
-    checkImg:require('IMG/user/goxuan@2x.png'),
-    periodList:[
-      {date:"周一 ",check:false},
-      {date:"周二 ",check:false},
-      {date:"周三 ",check:false},
-      {date:"周四 ",check:false},
-      {date:"周五 ",check:false},
-      {date:"周六 ",check:false},
-      {date:"周日 ",check:false}
-      ],
-      showBox:true,
-    date:"每天",
-    showDate:false,
-    itemDate:"",
-    endShow:false,
+  data: () => ({
+    checked: false,
+    detaliImg: require('IMG/user/arr.png'),
+    popupShow: false,
+    checkImg: require('IMG/user/goxuan@2x.png'),
+    periodList: [
+      { date: '周一 ', check: false },
+      { date: '周二 ', check: false },
+      { date: '周三 ', check: false },
+      { date: '周四 ', check: false },
+      { date: '周五 ', check: false },
+      { date: '周六 ', check: false },
+      { date: '周日 ', check: false }
+    ],
+    showBox: true,
+    date: '每天',
+    showDate: false,
+    itemDate: '',
+    endShow: false,
     endDate: '12:00',
-    endTime:null,
-    startShow:false,
+    endTime: null,
+    startShow: false,
     startDate: '12:00',
-    startTime:null,
-     greater:false
+    startTime: null,
+    greater: false
   }),
-  methods:{
-    onSwitch(checked){
-      this.checked=checked
+
+  created() {
+    this.checkHaveDisturb()
+  },
+
+  methods: {
+    onSwitch(checked) {
+      this.checked = checked
     },
-    periodHandle(){
+    periodHandle() {
       console.log(this.$refs.rr.innerText)
-      this.popupShow=!this.popupShow
-      if(
-        this.periodList[0].check===true
-      &&this.periodList[1].check===true
-      &&this.periodList[2].check===true
-      &&this.periodList[3].check===true
-      &&this.periodList[4].check===true
-      &&this.periodList[5].check===true
-      &&this.periodList[6].check===true
-      ){
-        this.showBox=false
-        this.showDate=true
-      }else{
-        this.showBox=true
-        this.showDate=false
+      this.popupShow = !this.popupShow
+      if (
+        this.periodList[0].check === true &&
+        this.periodList[1].check === true &&
+        this.periodList[2].check === true &&
+        this.periodList[3].check === true &&
+        this.periodList[4].check === true &&
+        this.periodList[5].check === true &&
+        this.periodList[6].check === true
+      ) {
+        this.showBox = false
+        this.showDate = true
+      } else {
+        this.showBox = true
+        this.showDate = false
       }
     },
-  setHandle(){
-    this.popupShow=!this.popupShow
-  },
-    periodTaget(index,item){
-        this.periodList[index].check = !this.periodList[index].check
+    setHandle() {
+      this.popupShow = !this.popupShow
+    },
+    periodTaget(index, item) {
+      this.periodList[index].check = !this.periodList[index].check
+      console.log(this.periodList)
     },
     // onChange(picker,value,index){
-    //   console.log(picker.getColumnValue(0),'-',picker.getColumnValue(1)) 
+    //   console.log(picker.getColumnValue(0),'-',picker.getColumnValue(1))
     // },
-    startHandle(){
-      this.startShow=!this.startShow
+    startHandle() {
+      this.startShow = !this.startShow
     },
-    onStartConfirm(N){
-      this.startTime=N
-      this.startShow=!this.startShow
+    onStartConfirm(N) {
+      this.startTime = N
+      this.startShow = !this.startShow
       console.log(this.startShow)
     },
-    onStartCancel(){
-      this.startShow=!this.startShow
+    onStartCancel() {
+      this.startShow = !this.startShow
       console.log(this.startShow)
     },
-    endHandle(){
-      this.endShow=!this.endShow
+    endHandle() {
+      this.endShow = !this.endShow
     },
-    onEndConfirm(N){
-      this.endTime=N
-      this.endShow=!this.endShow
-      if(this.endDate<this.startDate){
-        this.greater=true
+    onEndConfirm(N) {
+      this.endTime = N
+      this.endShow = !this.endShow
+      if (this.endDate < this.startDate) {
+        this.greater = true
         console.log(this.greater)
-      }else{
-        this.greater=false
+      } else {
+        this.greater = false
         console.log(this.greater)
       }
     },
-    onEndCancel(){
-      this.endShow=!this.endShow
+    onEndCancel() {
+      this.endShow = !this.endShow
+    },
+
+    //查询是否设置了勿扰模式
+    async checkHaveDisturb() {
+      const result = await userService.checkDisturbSetting()
+      // let tempList = this.periodList
+
+      if (result != null) {
+        let monday = result.monday
+        let tuesday = result.tuesday
+        let wednesday = result.wednesday
+        let thursday = result.thursday
+        let friday = result.friday
+        let saturday = result.saturday
+        let sunday = result.sunday
+        let beginTime = result.beginTime
+        let endTime = result.endTime
+
+        if (beginTime == '') {
+          this.startTime = ''
+          this.startDate = ''
+        } else {
+          this.startTime = beginTime
+          this.startDate = beginTime
+        }
+
+        if (endTime == '') {
+          this.endDate = ''
+          this.endTime = ''
+        } else {
+          this.endTime = endTime
+          this.endDate = endTime
+        }
+
+        if (monday == '') {
+          this.periodList[0].check = false
+        } else {
+          this.periodList[0].check = true
+        }
+        if (tuesday == '') {
+          this.periodList[1].check = false
+        } else {
+          this.periodList[1].check = true
+        }
+        if (wednesday == '') {
+          this.periodList[2].check = false
+        } else {
+          this.periodList[2].check = true
+        }
+        if (thursday == '') {
+          this.periodList[3].check = false
+        } else {
+          this.periodList[3].check = true
+        }
+        if (friday == '') {
+          this.periodList[4].check = false
+        } else {
+          this.periodList[4].check = true
+        }
+        if (saturday == '') {
+          this.periodList[5].check = false
+        } else {
+          this.periodList[5].check = true
+        }
+        if (sunday == '') {
+          this.periodList[6].check = false
+        } else {
+          this.periodList[6].check = true
+        }
+
+        //设置过勿扰模式
+        if (result.status == '0') {
+          this.checked = false
+        } else if (result.status == '1') {
+          this.checked = true
+        }
+      } else {
+        //没有设置过勿扰模式 开关进行关闭
+        this.checked = false
+      }
     }
   }
 }
 </script>
 <style lang="less">
-.no-disturb-page{
-  background:rgba(247,249,250,1);
-  .van-switch--on{
-    background:#007AE6;
+.no-disturb-page {
+  background: rgba(247, 249, 250, 1);
+  .van-switch--on {
+    background: #007ae6;
   }
-  .no-disturb-page-top{
-    margin-top:10px;
-    padding:18px 15px 7px 14px; 
-    background:rgba(255,255,255,1);
-    .disturb-box{
-      display: flex;
-      justify-content:space-between;
-      align-items:center;
-      margin-top:13px;
-      font-size:15px;
-      font-family:PingFangSC-Regular;
-      font-weight:400;
-      color:rgba(51,51,51,1);
-      line-height:21px;
-    }
-    .disturb-describe{
-      
-      font-size:13px;
-      padding:7px 16px 22px 15px;
-      background:rgba(247,249,250,1);
-      font-family:PingFangSC-Regular;
-      font-weight:400;
-      color:rgba(102,102,102,1);
-    }
-  }
-  .no-disturb-page-bottom{
-    background:rgba(255,255,255,1);
-    .cycle-set,.start-time,.end-time{
-      width:345px;
+  .no-disturb-page-top {
+    margin-top: 10px;
+    padding: 18px 15px 7px 14px;
+    background: rgba(255, 255, 255, 1);
+    .disturb-box {
       display: flex;
       justify-content: space-between;
-      height:56px;
-      margin-left:15px;
-      border-bottom:1px solid #E6E6E6;
       align-items: center;
-      p{
-        font-size:16px;
-        font-family:PingFangSC-Regular;
-        font-weight:400;
-        color:rgba(51,51,51,1);
+      margin-top: 13px;
+      font-size: 15px;
+      font-family: PingFangSC-Regular;
+      font-weight: 400;
+      color: rgba(51, 51, 51, 1);
+      line-height: 21px;
+    }
+    .disturb-describe {
+      font-size: 13px;
+      padding: 7px 16px 22px 15px;
+      background: rgba(247, 249, 250, 1);
+      font-family: PingFangSC-Regular;
+      font-weight: 400;
+      color: rgba(102, 102, 102, 1);
+    }
+  }
+  .no-disturb-page-bottom {
+    background: rgba(255, 255, 255, 1);
+    .cycle-set,
+    .start-time,
+    .end-time {
+      width: 345px;
+      display: flex;
+      justify-content: space-between;
+      height: 56px;
+      margin-left: 15px;
+      border-bottom: 1px solid #e6e6e6;
+      align-items: center;
+      p {
+        font-size: 16px;
+        font-family: PingFangSC-Regular;
+        font-weight: 400;
+        color: rgba(51, 51, 51, 1);
       }
-      .show-date,.right{
+      .show-date,
+      .right {
         display: flex;
-        font-size:16px;
-        font-family:PingFangSC-Regular;
-        font-weight:400;
-        color:rgba(153,153,153,1);
-        .show-box{
+        font-size: 16px;
+        font-family: PingFangSC-Regular;
+        font-weight: 400;
+        color: rgba(153, 153, 153, 1);
+        .show-box {
           display: flex;
         }
-        i{
-        margin-right:2px;
-        font-style: normal;
+        i {
+          margin-right: 2px;
+          font-style: normal;
         }
-        .icon{
-          margin-top:6px;
-          width:16px;
-          height:16px;
+        .icon {
+          margin-top: 6px;
+          width: 16px;
+          height: 16px;
         }
       }
-      }
-    .cycle-set{
-      .period-box{
-        li{
-          width:360px;
-          height:50px;
-          align-items:center;
-          margin-left:15px;
-          display:flex;
+    }
+    .cycle-set {
+      .period-box {
+        li {
+          width: 360px;
+          height: 50px;
+          align-items: center;
+          margin-left: 15px;
+          display: flex;
           justify-content: space-between;
-          font-size:15px;
-          font-family:PingFangSC-Regular;
-          font-weight:400;
-          color:rgba(51,51,51,1);
-          span{
-            width:22px;
-            height:22px;
+          font-size: 15px;
+          font-family: PingFangSC-Regular;
+          font-weight: 400;
+          color: rgba(51, 51, 51, 1);
+          span {
+            width: 22px;
+            height: 22px;
           }
         }
       }
-        .confirm-button{
-            border-top:6px solid #E8E8E8;
-            height:50px;
-            text-align:center;
-            line-height:50px;
-            font-size:16px;
-            font-family:PingFangSC-Regular;
-            font-weight:400;
-            color:rgba(51,51,51,1);
-          }
+      .confirm-button {
+        border-top: 6px solid #e8e8e8;
+        height: 50px;
+        text-align: center;
+        line-height: 50px;
+        font-size: 16px;
+        font-family: PingFangSC-Regular;
+        font-weight: 400;
+        color: rgba(51, 51, 51, 1);
+      }
     }
   }
 }
