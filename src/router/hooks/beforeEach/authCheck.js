53e739b707d174c36311a2a94af661db51a17278
suyanguo <350966631@qq.com>
2018-12-05 16:06:22 +0800

    支付调试

--- src/router/hooks/beforeEach/authCheck.js
+++ src/router/hooks/beforeEach/authCheck.js
@@ -28,39 +28,40 @@ export default async (to, from, next) => {
         if(parm.code){
             let cropId = localStorage.getItem('cropId')
             let payCorpId = store.getters.userInfo.payCorpId
-            if(payCorpId){// 通过payopenid返回的code
-                let userInfo = store.getters.userInfo
-                if(userInfo.payOpenId) {
-                    next()
-                    return
-                }
-                let pcOpenid = userInfo.pcOpenid
-                console.log(pcOpenid, 'pcOpenId')
-                console.log(parm.code, 'parm.code===')
-                const payopenIdObject = await commonService.getPayOpenId(parm.code, cropId, pcOpenid)
-                userInfo.payOpenId = payopenIdObject.payOpenId
-                store.dispatch('getUserInfo', userInfo)
-                console.log(payopenIdObject.payOpenId, 'payopenIdObject===')
-                next()
-            } else {
-                const wxAuthObject = await commonService.wxUserInfo(parm.code, cropId)
-                payCorpId = wxAuthObject.payCorpId
-                let userInfo = wxAuthObject.userInfo
-                userInfo.payCorpId = payCorpId
-                userInfo.cropId = cropId
-                userInfo.token = wxAuthObject.token
-                store.dispatch('getUserInfo', userInfo)
-                if(!userInfo.payOpenId) {//返回的payopenid为空，则从新授权获取
-                    console.log(wxAuthObject,'wxAuthObject=====')
-                    await localStorage.setItem('payCorpId', payCorpId)
-                    let wxurl = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + payCorpId 
-                        + '&redirect_uri=' + encodeURIComponent(wxredirecturl).toLowerCase() 
-                        + '&response_type=code&scope=snsapi_base&state=062882#wechat_redirect'
-                    window.location.href = wxurl;
-                    return
-                }
-                next()
-            }
+            let userInfo = store.getters.userInfo
+            console.log(userInfo,'payCorpId')
+            // if(payCorpId){// 通过payopenid返回的code
+            //     if(userInfo.payOpenId) {
+            //         next()
+            //         return
+            //     }
+            //     let pcOpenid = userInfo.pcOpenid
+            //     console.log(pcOpenid, 'pcOpenId')
+            //     console.log(parm.code, 'parm.code===')
+            //     const payopenIdObject = await commonService.getPayOpenId(parm.code, cropId, pcOpenid)
+            //     userInfo.payOpenId = payopenIdObject.payOpenId
+            //     store.dispatch('getUserInfo', userInfo)
+            //     console.log(payopenIdObject.payOpenId, 'payopenIdObject===')
+            //     next()
+            // } else {
+            //     const wxAuthObject = await commonService.wxUserInfo(parm.code, cropId)
+            //     payCorpId = wxAuthObject.payCorpId
+            //     let userInfo = wxAuthObject.userInfo
+            //     userInfo.payCorpId = payCorpId
+            //     userInfo.cropId = cropId
+            //     userInfo.token = wxAuthObject.token
+            //     store.dispatch('getUserInfo', userInfo)
+            //     if(!userInfo.payOpenId) {//返回的payopenid为空，则从新授权获取
+            //         console.log(wxAuthObject,'wxAuthObject=====')
+            //         await localStorage.setItem('payCorpId', payCorpId)
+            //         let wxurl = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + payCorpId 
+            //             + '&redirect_uri=' + encodeURIComponent(wxredirecturl).toLowerCase() 
+            //             + '&response_type=code&scope=snsapi_base&state=062882#wechat_redirect'
+            //         window.location.href = wxurl;
+            //         return
+            //     }
+            //     next()
+            // }
         } else {
             next()
         }
