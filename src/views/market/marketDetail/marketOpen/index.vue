898cd7dfb3f510640e3bf0deb219f727aae038ef
suyanguo <350966631@qq.com>
2018-12-06 18:26:57 +0800

    支付调试

--- src/views/market/marketDetail/marketOpen/index.vue
+++ src/views/market/marketDetail/marketOpen/index.vue
@@ -90,7 +90,7 @@ export default {
           nonceStr: res.nonceStr,
           package: res.packageId,
           signType: 'MD5',
-          paySign: res.signature,
+          paySign: res.signature.toLowerCase(),
           success: function(res) {
             console.log('支付suss')
           },
