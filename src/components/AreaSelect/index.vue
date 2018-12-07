81f3a6666b14538b22e76b5689c38a4191ae8609
火勺 <donocare@yeah.net>
2018-12-07 17:30:46 +0800

    城市选择问题添加

--- src/components/AreaSelect/index.vue
+++ src/components/AreaSelect/index.vue
@@ -1,6 +1,6 @@
 <template>
   <van-popup v-model="isShow" position="bottom">
-    <van-area :area-list="areaList" :value="value" @confirm="confirmHandler" />
+    <van-area :area-list="areaList" :value="value" @confirm="confirmHandler" @cancel="closePopup"/>
   </van-popup>
 </template>
 <script>
@@ -8,7 +8,12 @@ import { fullArea } from '@/utils/fullArea'
 export default {
   props: {
     show: { type: Boolean, default: false },
-    areaList: { type: Object, default: function () { return fullArea } },
+    areaList: {
+      type: Object,
+      default: function() {
+        return fullArea
+      }
+    },
     code: { type: String }
   },
   data: () => ({
@@ -16,21 +21,25 @@ export default {
     value: '110000'
   }),
   methods: {
-    confirmHandler (val) {
-      this.$emit('confirm', val)
+    confirmHandler(val) {
+      this.$emit('confirmCityData', val)
+      this.isShow = false
+    },
+    closePopup() {
+      this.isShow = false
     }
   },
   watch: {
-    show (val) {
+    show(val) {
       this.isShow = val
     },
-    isShow (val) {
+    isShow(val) {
       this.$emit('update:show', val)
     },
-    code (val) {
+    code(val) {
       this.value = val
     },
-    value (val) {
+    value(val) {
       this.$emit('update:code', val)
     }
   }
