257baa932351a21a52454d0f5510ed62e0c8ca61
suyanguo <350966631@qq.com>
2018-12-08 10:11:22 +0800

    pay

---
+++ src/views/market/couponSelect.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="user-myCoupon-page">
+    <coupon-item v-for="(item,index) in couponList" :key="index" :info="item"></coupon-item>
+  </div>
+</template>
+<script>
+import mycoupons from 'SERVICE/mycoupons'
+import CouponItem from 'COMP/User/myCoupon/CouponItem.vue'
+export default {
+  components: {
+    CouponItem
+  },
+  data: () => ({
+      couponList: []
+  }),
+
+  created() {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
