01a88e0f8f7b18be1d649aa606701c0bc4ee3289
zizi <zhangzihb@gmail.com>
2018-12-15 11:09:43 +0800

    superagent手机端问题修复

--- src/services/xhr/superAgent.js
+++ src/services/xhr/superAgent.js
@@ -6,16 +6,17 @@ import codeErrHandler from './codeErrHandler'
 import store from '@/store/'
 // codeHandleList 为{code: xxx, type: xxx}的对象数组，code表示需要处理的代号，type表示处理手法。
 //   当type: 0，默认处理形式；当type: 1，网络层不处理交由逻辑层处理；当type: 2，网络层处理好后再交由逻辑层处理。
-const xhr = ({url, body = {}, method = 'get', headers={},codeHandleList = null}) => {
+const xhr = ({ url, body = {}, method = 'get', headers = {}, codeHandleList = null }) => {
   // 参数处理
   url = url.replace(/\s+/g, '') // 去掉首尾空格
   method = method.toLowerCase()
   const host = process.env.VUE_APP_BASE_API_URL
   let head = Object.assign({
     'Content-Type': 'application/json; charset=UTF-8',
-    Authorization: store.getters.userInfo.token
-  },headers)
-  if (window.localStorage.getItem('deviceId')) head.uniquecode = window.localStorage.getItem('deviceId')
+  }, headers)
+  if (store.getters.userInfo && store.getters.userInfo.token) {
+    head.Authorization = store.getters.userInfo.token
+  }
   return new Promise((resolve, reject) => {
     let request = superAgent[method](host + url)//.withCredentials()
       .set(head)
