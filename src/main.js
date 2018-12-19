9244c69a591526c7e9e2624c0c775fd49e43f457
zizi <zhangzihb@gmail.com>
2018-12-19 18:27:24 +0800

    接口错误弹窗

--- src/main.js
+++ src/main.js
@@ -5,17 +5,11 @@ import router from '@/router/'
 import store from '@/store/'
 import '@/filters'
 import '@/directives'
-import '@/vant'
+import '@/vant'   // vant
 Vue.config.productionTip = false
 Vue.prototype.$devicePixelRatio = window.devicePixelRatio
-// vant
-import { Toast, Icon, PullRefresh, List, Popup, Search } from 'vant'
-Vue.use(Toast)
-Vue.use(Icon)
-Vue.use(PullRefresh)
-Vue.use(List)
-Vue.use(Popup)
-Vue.use(Search)
+
+
 // globle
 // wechatApi.init()
 
