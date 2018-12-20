bc10d5a3c3b52013bafeb9a0754150207e311974
zizi <zhangzihb@gmail.com>
2018-12-20 11:22:26 +0800

    热门楼盘

---
+++ src/components/Dynamics/HotEstateList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hot-estate-list">
+    <title-bar class="title-container" :conf="titleBarConf" v-if="list&&list.length>0"></title-bar>
+    <estate-item v-for="(item,index) in list" :key="index" :info="item" @click="godynamicsInfo(item)" @share="shareHandler(item)"></estate-item>
+  </div>
+</template>
+<script>
+import EstateItem from 'COMP/EstateItem'
+import TitleBar from 'COMP/TitleBar'
+
+export default {
+  components: {
+    TitleBar,
+    EstateItem
+  },
+  props: {
+    list: Array,
+    info: { type: Object }
+  },
+  data: () => ({
+    titleBarConf: {
+      title: '热门楼盘',
+      linkText: '更多楼盘',
+      link: '/market'
+    }
+  }),
+  methods: {
+    godynamicsInfo(item) {
+      this.$emit('click', item)
+    },
+    shareHandler(item) {
+      this.$emit('share', item)
+    }
+  }
+}
+</script>
--- src/views/dynamics/index.vue
+++ src/views/dynamics/index.vue
@@ -4,8 +4,11 @@
       <dynamics-collect :data="collectData" @click="goMessageInfo"/>
       <estate-recommend v-if="recommendData" :info="recommendData" @click="goRecommendInfo"/>
     </div>
-    <div class="list-container">
-      <my-estate-list :list="estateListData" @click="goRecommendInfo" @share="shareHandler" />
+    <div class="list-container" v-if="estateListData&&estateListData.length>0">
+      <my-estate-list :list="estateListData" @click="goRecommendInfo" @share="shareHandler"/>
+    </div>
+    <div class="list-container" v-if="hotEstateListData&&hotEstateListData.length>0">
+      <hot-estate-list :list="hotEstateListData" @click="goRecommendInfo" @share="shareHandler"/>
     </div>
   </div>
 </template>
@@ -13,19 +16,22 @@
 import DynamicsCollect from 'COMP/Dynamics/DynamicsCollect'
 import EstateRecommend from 'COMP/Dynamics/EstateRecommend'
 import MyEstateList from 'COMP/Dynamics/MyEstateList'
+import HotEstateList from 'COMP/Dynamics/HotEstateList'
 import CommonService from 'SERVICE/commonService'
 import dynamicsService from 'SERVICE/dynamicsService'
 export default {
   components: {
     DynamicsCollect,
     EstateRecommend,
-    MyEstateList
+    MyEstateList,
+    HotEstateList
   },
   data: () => ({
-    collectData: null,    // 数据中心数据
-    recommendData: null,  // 推荐盘数据
+    collectData: null, // 数据中心数据
+    recommendData: null, // 推荐盘数据
     estateListData: null, // 我的楼盘数据
-    timer:null
+    hotEstateListData: null,  // 热门楼盘数据
+    timer: null
   }),
   created() {
     let timeStamp = window.localStorage.getItem('timeStamp') || ''
@@ -35,22 +41,18 @@ export default {
     // 30s自动刷新数据
     this.timer = setInterval(() => {
       this.getCollectInfo()
-    },30000)
+    }, 30000)
   },
   methods: {
     //动态详情
     async goMessageInfo(num) {
-      console.log("num.customerCount.val",num.customerCount)
-      console.log("num.businessCardViews.val",num.businessCardViews)
-      console.log("num.estateViews.val",num.estateViews)
       if (num.customerCount != 0 || num.businessCardViews != 0 || num.estateViews != 0) {
-        // this.$router.push({ name: 'allDynamics', params: { customerCount: num.customerCount, businessCardViews: num.businessCardViews, estateViews: num.estateViews } })
         this.$router.push({ path: '/dynamics/allDynamics', query: { customerCount: num.customerCount, businessCardViews: num.businessCardViews, estateViews: num.estateViews } })
       }
     },
     //楼盘详情
     async goRecommendInfo(val) {
-      this.$router.push('/market/'+val.linkerId)
+      this.$router.push('/market/' + val.linkerId)
     },
     async getCollectInfo() {
       const res = await dynamicsService.getDynamicsCollect()
@@ -75,7 +77,8 @@ export default {
     async getEstateList() {
       const res = await dynamicsService.getEstateInfo()
       this.estateListData = res.myLinkerVOs
-      for(let temp of this.estateListData) {
+      this.hotEstateListData = res.hotLinkerVOs
+      for (let temp of this.estateListData) {
         temp.headImgUrl = temp.linkerHeadUrl
       }
       this.recommendData = res.aiLinkerVO
@@ -88,23 +91,20 @@ export default {
       }
     },
     shareHandler(info) {
-      // this.$router.push({name: 'market-share', params: {id: info.linkerId}})
-       this.$router.push({ name: 'marketDetail-share' })
+      this.$router.push({ name: 'marketDetail-share' })
     }
   },
-  beforeDestroy(){
+  beforeDestroy() {
     clearInterval(this.timer)
   }
 }
 </script>
 <style lang="less">
 .dynamics-page {
-  width: 100%;
-  height: 100%;
   background: #f7f9fa;
   .dynamics-top-container {
     background: #fff;
-    padding-bottom: 15px;
+    padding-bottom: 10px;
   }
   .list-container {
     background: #fff;
