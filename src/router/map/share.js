37e7f34541ee39cdd4a51ef8333c4183e630cf95
niebin0924 <804186194@qq.com>
2018-12-21 20:13:21 +0800

    注册修复

--- src/router/map/share.js
+++ src/router/map/share.js
@@ -1,14 +1,10 @@
 export default [
-    {
-      path: '/article/:id/:city',
-      name: 'article-detail',
-      component: resolve => require(['@/views/share/articleDetail.vue'], resolve),
-      meta: {
-        title: '文章',
-        tabbar: {
-          show: false
-        }
-      }
+  {
+    path: '/article/:id/:city',
+    name: 'article-detail',
+    component: resolve => require(['@/views/share/articleDetail.vue'], resolve),
+    meta: {
+      title: '文章',
     }
-  ]
-  
+  }
+]
