3f433a2ceec4bb3a7ae0e365321e5a7f2d208d30
suyanguo <350966631@qq.com>
2018-12-23 17:48:49 +0800

    fix

---
+++ src/store/modules/dynamics.js
@@ -0,0 +1,27 @@
+import * as types from '@/store/mutation-types'
+const state = {
+    currDataDynamicsTab: 0
+}
+
+const getters = {
+    currDataDynamicsTab: state => {
+        return state.currDataDynamicsTab
+    },
+}
+
+const actions = {
+
+}
+
+const mutations = {
+  [types.CURR_DATA_DYNAMICS_TAB](state, currDataDynamicsTab) {
+    state.currDataDynamicsTab = currDataDynamicsTab
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
