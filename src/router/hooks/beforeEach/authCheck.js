d03f72c659a705d8be538bf53194a607976e5529
suyanguo <350966631@qq.com>
2018-12-28 10:14:38 +0800

    update

--- src/router/hooks/beforeEach/authCheck.js
+++ src/router/hooks/beforeEach/authCheck.js
@@ -41,8 +41,8 @@ export default async (to, from, next) => {
         // 获取jssdk授权
         if (!store.getters.jssdkConfig || !store.getters.jssdkConfig.signature) {
           try {
-            alert(location.href)
-            window.awHelper.wechatHelper.init()
+            // alert(location.href)
+            // window.awHelper.wechatHelper.init()
           } catch (e) {
             console.log('[error:window.awHelper.wechatHelper]')
             next()
