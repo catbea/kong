770c4d1e5b2883f3f2a5a3524d5fb67898788308
niebin0924 <804186194@qq.com>
2018-12-28 17:27:03 +0800

    隐藏分享按钮

--- src/views/discover/detail.vue
+++ src/views/discover/detail.vue
@@ -153,7 +153,7 @@ export default {
     shareData: null
   }),
   created() {
-    window.awHelper.wechatHelper.wx.showMenuItems()
+    window.awHelper.wechatHelper.wx.showAllNonBaseMenuItem()
     this.id = this.$route.params.id
     this.city = this.$route.params.city
     this.agentId = this.$route.query.agentId
