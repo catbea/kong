27e02a869111f063e63a43baef92c3dee0941ce2
suyanguo <350966631@qq.com>
2018-12-29 18:03:59 +0800

    fix

--- src/router/hooks/beforeEach/authCheck.js
+++ src/router/hooks/beforeEach/authCheck.js
@@ -108,20 +108,20 @@ export default async (to, from, next) => {
           window.location.href = wxurl
           return
         } else {
-          // if (!store.getters.jssdkConfig || !store.getters.jssdkConfig.signature) {
-          //   try {
-          //     if(isIOS()) {
-          //       if(to.path == '/'){
-          //         window.awHelper.wechatHelper.init()
-          //       }
-          //     } else {
-          //       window.awHelper.wechatHelper.init()
-          //     }
-          //   } catch (e) {
-          //     console.log('[error:window.awHelper.wechatHelper]')
-          //     next()
-          //   }
-          // }
+          if (!store.getters.jssdkConfig || !store.getters.jssdkConfig.signature) {
+            try {
+              if(isIOS()) {
+                if(to.path == '/'){
+                  window.awHelper.wechatHelper.init()
+                }
+              } else {
+                window.awHelper.wechatHelper.init()
+              }
+            } catch (e) {
+              console.log('[error:window.awHelper.wechatHelper]')
+              next()
+            }
+          }
         }
         next()
       }
