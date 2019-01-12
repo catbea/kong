327f7409c3409a70180904824a05e2d19a88047b
以梦为马 <532079124@qq.com>
2019-01-12 21:31:51 +0800

    文章列表

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,6 +1,43 @@
 <template>
-  <div class="article-list">
+  <div class="article-box">
     <Guide v-if="showGuide" @hideGuide="hideStep"/>
+    <div class="tab-bar scale-1px-bottom">
+      <span class="recommend">推荐</span>
+      <span>深圳</span>
+      <span>行情</span>
+      <span>大佬说</span>
+    </div>
+    <div class="article-list">
+      <div class="article-item">
+        <div class="content scale-1px-bottom">
+          <div class="left-cnt">
+            <h3 class="title"></h3>
+            <div class="attr">
+              <p class="source">
+                <span class="name"></span>
+                <span class="time"></span>
+              </p>
+              <span class="read"></span>
+            </div>
+          </div>
+          <div class="img"></div>
+        </div>
+        <div class="comment">
+          <div class="like-cnt">
+            <div class="like-box">
+              <span class="name"></span>
+            </div>
+            <div class="comment-box">
+              <p class="comment-item"></p>
+            </div>
+          </div>
+          <div class="action">
+            <span class="like-icon"></span>
+            <span class="comment-icon"></span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,3 +66,25 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+.article-box{
+  .tab-bar{
+    font-size: 14px;
+    color: #333;
+    height: 20px;
+    line-height: 20px;
+    padding: 12px 16px;
+    height: 54px;
+    span{
+      display: inline-block;
+      margin-right: 32px;
+    }
+    .recommend{
+      font-size: 24px;
+      height: 34px;
+      line-height: 34px;
+    }
+  }
+}
+</style>
