5ce5a7db9a2ab029c7cbee8a4a707c8c6f03ab2c
火勺 <donocare@yeah.net>
2019-01-15 18:09:52 +0800

    添加文章链接的页面UI

--- src/views/discover/newlyAdded/index.vue
+++ src/views/discover/newlyAdded/index.vue
@@ -2,11 +2,19 @@
     <div class="page-body">
         <div class="copyTitle">请复制原文链接</div>
         <div class="notice-view">目前仅支持爬取微信公众号内容，如有侵权行为，发布人将承担相关责任</div>
+        <textarea class="linker-input" :placeholder="defaultText"></textarea>
+        <div class="clear-address">清空地址</div>
+        <div class="add-article">添加文章</div>
+        <div class="start-edit">开始编辑</div>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    defaultText: '请点击喜欢的微信公众号文章右上角更多进行复制。并粘贴到这里'
+  })
+}
 </script>
 <style lang="less">
 .page-body {
@@ -27,6 +35,58 @@ export default {}
     margin-left: 16px;
     margin-top: 5px;
     margin-right: 16px;
+  }
+
+  > .linker-input {
+    width: 90%;
+    margin-left: 5%;
+    margin-right: 5%;
+    border: #e4e6f0 1px solid;
+    margin-top: 46px;
+    height: 126px;
+    padding: 10px;
+    color: #c8cacc;
+    font-size: 16px;
+  }
+
+  > .clear-address {
+    margin-top: 40px;
+    width: 90%;
+    margin-left: 5%;
+    margin-right: 5%;
+    background: rgba(242, 248, 254, 1);
+    border-radius: 6px;
+    font-size: 16px;
+    text-align: center;
+    line-height: 44px;
+    color: #445166;
+  }
+
+  > .add-article {
+    margin-top: 20px;
+    width: 90%;
+    margin-left: 5%;
+    margin-right: 5%;
+    background: rgba(242, 248, 254, 1);
+    border-radius: 6px;
+    font-size: 16px;
+    text-align: center;
+    line-height: 44px;
+    color: #445166;
+  }
+
+  > .start-edit {
+    margin-top: 20px;
+    width: 90%;
+    margin-left: 5%;
+    margin-right: 5%;
+    background: rgba(0, 122, 230, 1);
+    border-radius: 6px;
+    height: 44px;
+    color: #ffffff;
+    font-size: 16px;
+    text-align: center;
+    line-height: 44px;
   }
 }
 </style>
