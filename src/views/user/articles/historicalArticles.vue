a5d0ea67a20d0936fe6e355f4978128c5fc47fb3
火勺 <donocare@yeah.net>
2019-01-16 15:38:37 +0800

    添加文章成功跳转到写一写文章编辑页面

--- src/views/user/articles/historicalArticles.vue
+++ src/views/user/articles/historicalArticles.vue
@@ -5,6 +5,7 @@
         @clickShare="clickShare"
         @clickEdit="clickEdit"
         @clickCollection="clickCollection"
+        :toSelectTap='typeCode'
       ></select-tab>
     </div>
     <div class="list-result" v-if="typeCode=='1'">
@@ -80,7 +81,15 @@ export default {
   computed: {
     ...mapGetters(['userInfo'])
   },
-  created() {},
+  created() {
+
+   this.typeCode = this.$route.query.typeCode
+
+   if(this.typeCode=='3'){
+      this.clickEdit('3');
+   }
+
+  },
   methods: {
     clickShare(val) {
       this.typeCode = val
