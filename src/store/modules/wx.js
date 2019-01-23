86a648ae0b60e6a2bbdf4e7fb7a68e5bf93676fa
niebin0924 <804186194@qq.com>
2019-01-23 22:38:06 +0800

    恢复

--- src/store/modules/wx.js
+++ src/store/modules/wx.js
@@ -18,7 +18,7 @@ const actions = {
     // alert('wxTicket get done'+result.appId)
     const conf = {
       beta: true, // 必须这么写，否则wx.invoke调用形式的jsapi会有问题
-      debug: true, // __DEV__ 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      debug: false, // __DEV__ 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: result.appId, // 必填，公众号的唯一标识
       timestamp: result.timestamp, // 必填，生成签名的时间戳
       nonceStr: result.nonceStr, // 必填，生成签名的随机串
