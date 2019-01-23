d6db0f9f8fa93d980d7f933761777cbc3541d229
以梦为马 <532079124@qq.com>
2019-01-23 19:40:23 +0800

    ios橡皮筋效果

--- src/views/share/articleDetail.vue
+++ src/views/share/articleDetail.vue
@@ -206,7 +206,9 @@ export default {
     cardQrInfo: null,
     marketQrInfo: null,
     articleQrInfo: null,
-    shareUuid: '' // 分享ID
+    shareUuid: '', // 分享ID
+    startY: '',
+    endY: ''
   }),
   created() {
     window.awHelper.wechatHelper.wx.showOptionMenu()
@@ -450,6 +452,24 @@ export default {
     $route() {
       location.reload()
     }
+  },
+  mounted () {
+    document.querySelector('.discover-detail-container').addEventListener('touchstart', (e) => {
+      this.startY = e.changedTouches[0].pageY
+    })
+    document.querySelector('.discover-detail-container').addEventListener('touchmove', (e) => {
+      this.endY = e.changedTouches[0].pageY
+      let scrollHeight = document.querySelector('.discover-detail-container').scrollHeight // 元素高度
+      let scrollTop = document.querySelector('.discover-detail-container').scrollTop // 滚动高度
+      let clientHeight = document.querySelector('.discover-detail-container').clientHeight // 可视高度
+      if (scrollTop===0 && endY - startY > 10) {
+         e.preventDefault()
+      }
+      if (scrollHeight <= scrollTop + clientHeight && startY - endY > 10) {
+        e.preventDefault()
+      }
+    }, { passive: false })
+    
   }
 }
 </script>
