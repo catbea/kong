d297e2bc91e8335c60cf9359c7966a46e0000bf2
suyanguo <350966631@qq.com>
2019-01-25 14:25:27 +0800

    分享标题更改

--- src/views/share/articleDetail.vue
+++ src/views/share/articleDetail.vue
@@ -265,8 +265,8 @@ export default {
       let host = process.env.VUE_APP_APP_URL
       host = host + '#/article/' + this.infoId + '/' + encodeURI(this.city) + '?agentId=' + this.info.agentId + '&enterpriseId=' + this.enterpriseId + '&shareUuid=' + this.shareUuid
       this.friendShareData = {
-        title: 'AW大师写一写',
-        desc: this.info.title,
+        title: this.info.title,
+        desc: '内容来源：AW大师写一写',
         imgUrl: this.info.image,
         link: host
       }
