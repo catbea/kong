a5ae8b41deadebe473314865e7f8b67ee9ffbb21
suyanguo <350966631@qq.com>
2019-02-23 15:53:54 +0800

    auth

--- src/views/share/articleDetail.vue
+++ src/views/share/articleDetail.vue
@@ -204,7 +204,7 @@ export default {
   },
   methods: {
     checkAuth() {
-      let wxredirecturl = window.location.href.replace('?from=singlemessage', '').split('?')[0]
+      let wxredirecturl = 'https://sit2.zooming-data.com'//window.location.href.replace('?from=singlemessage', '').split('?')[0]
       let parm = this.getUrlQueryParams(location.href)
       console.log(parm)
       if(parm.code) {
