186f3d9f5c4d645a3b55ef87bd7dc4a5f5ba7cab
以梦为马 <532079124@qq.com>
2019-02-28 15:24:21 +0800

    滑动组件测试

--- src/components/Custom/MyCustomItem.vue
+++ src/components/Custom/MyCustomItem.vue
@@ -1,5 +1,26 @@
 <template>
-  <van-swipe-cell :right-width="80" :on-close="onClose">          
+  <swipe-cell ref="swipecell">
+    <div class="content" slot="content">
+      <div class="van-hairline--bottom my-custom-item" @click="clickHandler">
+        <avatar class="user-avatar" :avatar="info.avatarUrl"></avatar>
+        <div
+          class="bg_img user-attention"
+          :style="{backgroundImage:'url('+attentionImg+')'}"
+          v-if="info.attentionStatus==0"
+        ></div>
+        <div class="baseinfo-box">
+          <p class="username">{{info.clientName}}</p>
+          <p class="base-focus">{{focusInfo}}</p>
+        </div>
+        <div class="intention-box">
+          <p class="intention-value" v-bind:style="{'color':Number(this.info.intentionality*100) >70?'#007AE6':'#999999'}">{{Number(this.info.intentionality*100).toFixed(1)}}%</p>
+          <p class="intention-title">意向度</p>
+        </div>
+      </div>
+    </div>
+    <button @click="onClose" style="background:#EA4D2E">删除</button>
+  </swipe-cell>
+  <!-- <van-swipe-cell :right-width="80" :on-close="onClose">          
     <van-cell-group>      
       <div class="van-hairline--bottom my-custom-item" @click="clickHandler">
         <avatar class="user-avatar" :avatar="info.avatarUrl"></avatar>
@@ -19,13 +40,15 @@
       </div>
     </van-cell-group>
     <span slot="right" class="delete-btn">删除</span>
-  </van-swipe-cell>
+  </van-swipe-cell> -->
 </template>
 <script>
 import Avatar from 'COMP/Avatar'
+import swipeCell from 'vue-swipe-cell'
 export default {
   components: {
-    Avatar
+    Avatar,
+    swipeCell
   },
   props: {
     info: Object,
@@ -41,27 +64,39 @@ export default {
     confirm () {
       this.$emit('delete', this.num)
     },
-    onClose(clickPosition, instance) {
-      switch (clickPosition) {
-        case 'left':
-        case 'cell':
-        case 'outside':
-          instance.close();
-          break;
-        case 'right':
-           this.$dialog.confirm({
-              title: '是否确定移除该客户',
-              message: '删除客户,也会删除与TA的聊天记录',
-              className: 'delete-dialog'
-          }).then(() => {
-            this.confirm()
-            instance.close()
-          }).catch(() => {
-            instance.close()
-          });
-          break
-      }
+    onClose () {
+      this.$dialog.confirm({
+          title: '是否确定移除该客户',
+          message: '删除客户,也会删除与TA的聊天记录',
+          className: 'delete-dialog'
+      }).then(() => {
+        this.confirm()
+        this.$refs.swipecell.initAllCell()
+      }).catch(() => {
+        this.$refs.swipecell.initAllCell()
+      })
     }
+    // onClose(clickPosition, instance) {
+    //   switch (clickPosition) {
+    //     case 'left':
+    //     case 'cell':
+    //     case 'outside':
+    //       instance.close();
+    //       break;
+    //     case 'right':
+    //        this.$dialog.confirm({
+    //           title: '是否确定移除该客户',
+    //           message: '删除客户,也会删除与TA的聊天记录',
+    //           className: 'delete-dialog'
+    //       }).then(() => {
+    //         this.confirm()
+    //         instance.close()
+    //       }).catch(() => {
+    //         instance.close()
+    //       })
+    //       break
+    //   }
+    // }
   },
   computed: {
     focusInfo() {
@@ -181,5 +216,11 @@ export default {
     -webkit-overflow-scrolling: touch;
     white-space: pre-wrap;
   }
+}
+.h-cell-swipe a .cell-content{
+  padding: 0!important;
+}
+.van-hairline--bottom::after{
+  border-bottom-width: 0;
 }
 </style>
