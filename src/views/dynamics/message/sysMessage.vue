00463143315ab40e3f8cc2056fdd28641451284c
火勺 <donocare@yeah.net>
2019-03-14 15:07:16 +0800

    系统消息列表添加图标

--- src/views/dynamics/message/sysMessage.vue
+++ src/views/dynamics/message/sysMessage.vue
@@ -8,18 +8,18 @@
             <div slot="container">
               <div class="sys-shadowBox">
                 <p class="sys-shadowBox-title">{{item.title}}</p>
-                <p class="sys-shadowBox-time" v-html="item.content.replace(/(\r+\n+)|(\n+)/g,'<br>')"></p>
-                <p class="look-detail" v-show="detailShow">查看详情</p>
-                <div style="display:flex;" v-show="appointment">
-                  <p class="client-detail">客户详情</p>
-                  <p class="go-report">立即报备</p>
-                </div>
+                <p
+                  class="sys-shadowBox-time"
+                  v-html="item.content.replace(/(\r+\n+)|(\n+)/g,'<br>')"
+                ></p>
                 <!-- <p class="sys-shadowBox-remarks">本次主要更新内容有： 1.增加勿扰模式 2.VIP功能优化调整 3.我的楼盘增加关闭展示功能</p> -->
                 <!-- <div class="button-item">
                   <div class="left-button">客户详情</div>
                   <div class="right-button">立即报备</div>
                 </div>-->
-                <div class="button-detail">查看详情 <p>你好</p></div> 
+                <div class="button-detail" @click="enterActivityDetail">查看详情
+                  <img :src="detailIcon">
+                </div>
               </div>
             </div>
           </shadow-box>
@@ -43,33 +43,24 @@ export default {
       size: 10,
       current: 1,
       pages: 1,
-      appointment:false,//是否显示预约按钮
-      detailShow:false,//是否显示查看详情按钮
+      detailIcon: require('IMG/activity/blue_arrow.png')
     }
   },
   created() {
     // this.getSystemMessageList()
   },
   methods: {
-    domShow(text){//根据字符串显示dom
-      switch (text) {
-        case '恭喜获奖':
-          this.detailShow=true
-          break;
-        case '客户预约':
-          this.appointment=true
-          break;
-        default:
-          break;
-      }
-    },
     async getSystemMessageList() {
-      const res = await dynamicsService.getSystemMessage({current: this.current, size: this.size})
-      console.log(res)
+      const res = await dynamicsService.getSystemMessage({ current: this.current, size: this.size })
       this.pages = res.pages
       this.sysMessage.push(...res.records)
       this.current += 1
     },
+
+    enterActivityDetail() {
+      this.$router.push('/activity/partake')
+    },
+
     // 加载更多
     async onLoad() {
       if (this.current > this.pages) {
@@ -101,7 +92,56 @@ export default {
     }
     .sys-shadowBox {
       padding: 16px;
-      position: relative;
+
+      .button-detail {
+        width: 100%;
+        height: 40px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #077de6;
+        font-size: 14px;
+
+        img {
+          width: 12px;
+          height: 12px;
+          margin-left: 6px;
+        }
+      }
+
+      .button-item {
+        width: 100%;
+        height: 40px;
+        display: flex;
+        justify-content: space-between;
+
+        .left-button {
+          margin-right: 12px;
+          height: 40px;
+          width: 50%;
+          background: rgba(242, 244, 245, 1);
+          border-radius: 4px;
+          color: #333333;
+          font-size: 14px;
+          justify-content: center;
+          align-items: center;
+          display: flex;
+        }
+
+        .right-button {
+          margin-left: 12px;
+          height: 40px;
+          width: 50%;
+          background: rgba(242, 244, 245, 1);
+          border-radius: 4px;
+          font-size: 14px;
+          color: #077de6;
+          justify-content: center;
+          align-items: center;
+          display: flex;
+        }
+      }
+
       .sys-shadowBox-title {
         font-size: 20px;
         font-weight: 600;
@@ -118,54 +158,11 @@ export default {
       }
       .sys-shadowBox-remarks {
         font-size: 14px;
-
         font-weight: 400;
         color: rgba(51, 51, 51, 1);
         line-height: 21px;
-      }
-      .look-detail{
-        font-size:14px;
-        font-family:PingFang-SC-Regular;
-        font-weight:400;
-        color:rgba(7,125,230,1);
-        text-align: center;
-        i{
-          display:inline-block;
-          width:12px;
-          height:12px;
-        }
-      }
-      .client-detail{
-        width:143px;
-        height:40px;
-        background:rgba(242,244,245,1);
-        border-radius:4px;
-        font-size:14px;
-        font-family:PingFang-SC-Medium;
-        font-weight:600;
-        color:rgba(51,51,51,1);
-        line-height:40px;
-        text-align:center;
-      }
-      .go-report{
-        margin-left:24px;
-        width:143px;
-        height:40px;
-        background:rgba(242,244,245,1);
-        border-radius:4px;
-        font-size:14px;
-        font-family:PingFang-SC-Medium;
-        font-weight:500;
-        color:rgba(7,125,230,1);
-        line-height:40px;
-        text-align:center;
       }
     }
   }
 }
 </style>
-
-
-
-
- 
