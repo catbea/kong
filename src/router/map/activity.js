1d6ed7c313326ed86bcd44bf18f0484098e6877e
niebin0924 <804186194@qq.com>
2019-03-14 10:41:41 +0800

    路由

--- src/router/map/activity.js
+++ src/router/map/activity.js
@@ -6,18 +6,29 @@ export default [
         meta: {
             title: '活动券',
             tabbar: {
-                show: true
+                show: false
             }
         }
     },
     {
-        path: '/partake',
-        name: 'partake',
+        path: '/activity/partake',
+        name: 'activity-partake',
         component: resolve => require(['@/views/activity/partakeSuccess.vue'], resolve),
         meta: {
             title: '恭喜获得',
             tabbar: {
-                show: true
+                show: false
+            }
+        }
+    },
+    {
+        path: '/activity/qrcode',
+        name: 'activity-qrcode',
+        component: resolve => require(['@/views/activity/scanQRCode.vue'], resolve),
+        meta: {
+            title: '识别二维码',
+            tabbar: {
+                show: false
             }
         }
     }
---
+++ src/views/activity/scanQRCode.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="scan-code-page"></div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less" scoped>
+.scan-code-page {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(180deg, rgba(255, 145, 0, 1) 0%, rgba(254, 91, 1, 1) 38%, rgba(255, 62, 0, 1) 73%, rgba(250, 115, 2, 1) 100%);
+}
+</style>
+
+
