b21b627ffe4f55375adfd63036c2c4446ac173aa
suyanguo <350966631@qq.com>
2019-03-27 10:43:49 +0800

    share test

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -50,7 +50,7 @@ const state = {
     ],
     leavingStatus: 0,
     token:
-      'eyJhbGciOiJIUzUxMiJ9.eyJyYW5kb21LZXkiOiIzNWk2NDQiLCJzdWIiOiJhZ2VudElkOnd3NWRhNjQ0NzMwNTYyN2ViMTo2MTMiLCJleHAiOjE1NTQxMTcxNzMsImlhdCI6MTU1MzUxMjM3M30.Yf_wL2N4cr9QvVESv78dqWKN9PSDDAR5fjlI8Ji9G2PpseZVSaLFjkT7jObUMiSUok7PUXB_9dX1QwkG_b6LDw',
+      'eyJhbGciOiJIUzUxMiJ9.eyJyYW5kb21LZXkiOiIxemdkMWwiLCJzdWIiOiJhZ2VudElkOnd3OGY2ODAxYmE1ZmQyYTExMjo0MTQ5IiwiZXhwIjoxNTU0MjU4NzY3LCJpYXQiOjE1NTM2NTM5Njd9.6jvIg30_ftVhgZvgnUn0mfLD8BM4zQxpW6v_Xk_RJbvY8m7QAI9ZaFxPdu6EwbimcJqB7lb5Y_JtX2RLT400lw',
     majorCity: '太原市',
     majorRegion: '广东省/深圳市/南山区',
     name: '周丹Nike',
