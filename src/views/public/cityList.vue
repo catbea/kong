ab727f507101623be61393ce32942515cdec42ce
以梦为马 <532079124@qq.com>
2019-03-29 10:26:18 +0800

    城市定位

--- src/views/public/cityList.vue
+++ src/views/public/cityList.vue
@@ -188,17 +188,18 @@ export default {
     },
     // 重新定位
     retryLocation() {
+      let _this = this
       wx.getLocation({
         type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
         success: res => {
           console.log('11111',res)
-          this.getCity(res)
+          _this.getCity(res)
         },
         fail: () => {
-          this.$toast('定位失败')
+          _this.$toast('定位失败')
         },
         cancel: res => {
-          this.$toast('定位取消')
+          _this.$toast('定位取消')
         }
       })
     },
