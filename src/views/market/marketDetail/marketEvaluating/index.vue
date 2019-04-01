98d11e5783a743640010d68e6cbeec1e2a4608af
火勺 <donocare@yeah.net>
2019-04-01 11:05:31 +0800

    11

--- src/views/market/marketDetail/marketEvaluating/index.vue
+++ src/views/market/marketDetail/marketEvaluating/index.vue
@@ -1,20 +1,36 @@
 <template>
-    <div class="html-body">
-        <div v-html="htmlContent"></div>
-        <div class="thumbs-up">
-            <img class="thumb-img" :src="thumbImg">
-            <span class="thumb-num">赞(32)</span>
-        </div>
+  <div class="html-body">
+    <div v-html="htmlContent"></div>
+    <div class="thumbs-up">
+      <img class="thumb-img" :src="thumbImg">
+      <span class="thumb-num">赞(32)</span>
     </div>
+  </div>
 </template>
 
 
 <script>
+import marketService from 'SERVICE/marketService'
+
 export default {
   data: () => ({
     htmlContent: '',
     thumbImg: require('IMG/market/thumb.png')
-  })
+  }),
+
+  created() {
+    this.getEvaluatingDetail('1')
+  },
+
+  methods: {
+    async getEvaluatingDetail(reviewId) {
+      //获取评测详情
+      const result = await marketService.getEvaluatingDetail(reviewId)
+      console.log(result)
+
+
+    }
+  }
 }
 </script>
 
@@ -32,7 +48,7 @@ export default {
     height: 80px;
     background: rgba(250, 133, 72, 1);
     border-radius: 50%;
-    align-items: center;    
+    align-items: center;
     flex-direction: column;
 
     .thumb-img {
@@ -41,10 +57,10 @@ export default {
       height: 33px;
     }
 
-    .thumb-num{
-        font-size: 12px;
-        color: #ffffff;
-        margin-top: 5px;
+    .thumb-num {
+      font-size: 12px;
+      color: #ffffff;
+      margin-top: 5px;
     }
   }
 }
