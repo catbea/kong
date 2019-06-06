8fdfcecd0629efbd60bb4fda52ccdd1dde07c244
以梦为马 <532079124@qq.com>
2019-06-06 16:35:10 +0800

    热门楼盘

--- src/components/Dynamics/HotEstateList.vue
+++ src/components/Dynamics/HotEstateList.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="hot-estate-list">
     <title-bar class="title-container" :conf="titleBarConf" v-if="list&&list.length>0"></title-bar>
-    <market-describe v-for="(item,index) in list" :key="index" :itemInfo="item" @skipDetail="godynamicsInfo(item)" @openReturnHandle="openHandler(item)"></market-describe>
+    <market-describe v-for="(item,index) in list" :key="index" :itemInfo="item" :vipInfo="vipInfo"  @skipDetail="godynamicsInfo(item)" @openReturnHandle="openHandler(item)"></market-describe>
   </div>
 </template>
 <script>
 import MarketDescribe from 'COMP/MarketDescribe'
 import TitleBar from 'COMP/TitleBar'
-
+import marketService from 'SERVICE/marketService'
 export default {
   components: {
     TitleBar,
@@ -22,14 +22,23 @@ export default {
       title: '热门楼盘',
       linkText: '更多楼盘',
       link: '/market'
-    }
+    },
+    vipInfo: {}
   }),
+  created () {
+    this.getVipInfo()
+  },
   methods: {
     godynamicsInfo(item) {
       this.$emit('click', item)
     },
     openHandler(item) {
       this.$emit('open', item)
+    },
+    // 获取VIP详情
+    async getVipInfo() {
+      let res = await marketService.vipInfo()
+      this.vipInfo = res
     }
   },
   watch: {
