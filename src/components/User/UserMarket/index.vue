c35450971c8065b3a519630b22bfa447a26e4b4b
以梦为马 <532079124@qq.com>
2019-06-20 16:30:35 +0800

    bug fix

--- src/components/User/UserMarket/index.vue
+++ src/components/User/UserMarket/index.vue
@@ -22,7 +22,7 @@
           <li>
             <div class="tag-item-statu blue" v-if="0===dataArr.saleStatus">{{status[dataArr.saleStatus]}}</div>
             <div class="tag-item-statu red" v-if="1===dataArr.saleStatus">{{status[dataArr.saleStatus]}}</div>
-            <div class="tag-item-statu gary" v-if="3===dataArr.saleStatus">{{status[dataArr.saleStatus]}}</div>
+            <div class="tag-item-statu gary" v-if="3===dataArr.saleStatus">售罄</div>
             <div class="tag-item" v-for="(item,index) in dataArr.linkerTags ? dataArr.linkerTags.slice(0,2):[]" :key="index">{{item}}</div>
           </li>
           <li>
